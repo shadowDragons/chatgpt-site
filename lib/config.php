6fc20846dc3154caad9a61bed5bd8597cdaadb83
shadowDragons <shadow_dragon@126.com>
2024-02-03 21:14:04 +0800

    update

--- lib/config.php
+++ lib/config.php
@@ -3,9 +3,9 @@
 //配置数据库信息
 $cfg = array(
 'host' => 'localhost',
-'user' => 'chatgpt', //数据库用户名
-'pwd' => 'chatgpt',  //密码
-'db' => 'chatgpt',  //数据库名
+'user' => 'chatgpt_studygee', //数据库用户名
+'pwd' => 'mYBbbipN2jtGwCSC',  //密码
+'db' => 'chatgpt_studygee',  //数据库名
 'charset' => 'utf8', 
 'salt'=>'L&#7sd":Adfqef]', 
 'zf'=>'wuxinyue'
